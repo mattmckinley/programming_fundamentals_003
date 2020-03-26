22e0813a928797aa6401b645a013406549a94343
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-03-26 16:33:38 +0000

    work upto Q8finished

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,25 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Assassination of Margaret Thatcher"
+        )
+      ).toBe(true);
+    });
+    test("returns false if the book doesnt exist", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Hobbit"
+        )
+      ).toBe(false);
+    });
+  });
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("return the total number of books that begin with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+    });
+  });
 });
